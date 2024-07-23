92e7803c755aff9c8430c717117fd77eab25014e
Rasum771 <aydarusmohey12@gmail.com>
2024-07-22 21:30:09 -0500

    Got Scoreboard setup

--- index.py
+++ index.py
@@ -1,9 +1,12 @@
 import pygame
 import random
-
 pygame.init()
 
 # Initialize variables
+Score = 0
+
+font = pygame.font.Font(None,80)
+
 running = True
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
@@ -12,37 +15,39 @@ clock = pygame.time.Clock()
 pos = pygame.Vector2(random.randrange(100, 1100), random.randrange(100, 700))
 
 # Time variables
-spawn_rate = 1000 # Spawn rate in milliseconds (1000 ms = 1 second)
+spawn_rate = 700 # Spawn rate in milliseconds (1000 ms = 1 second)
 last_spawn_time = pygame.time.get_ticks()  # Get the current time in milliseconds
 
 while running:
     current_time = pygame.time.get_ticks()  # Get the current time in milliseconds
-
+    if current_time - last_spawn_time > spawn_rate:
+        pos = pygame.Vector2(random.randrange(100, 1100), random.randrange(100, 700))
+        last_spawn_time = current_time  # Update the last spawn time
     # Process player inputs
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        elif event.type == pygame.MOUSEBUTTONUP:
+            poss = pygame.Vector2(event.pos)
+            distance = poss.distance_to(pos)
+
+            if distance < 40:
+                print("+1")
+                Score += 1
 
     # Check if it's time to spawn a new circle
-    if current_time - last_spawn_time > spawn_rate:
-       pos = pygame.Vector2(random.randrange(100, 1100), random.randrange(100, 700))
-       last_spawn_time = current_time  # Update the last spawn time
+
 
     # Do logical updates here
-    # ...
-
+    # ...  
     # event listener for click
     ev = pygame.event.get()
-
-    for event in ev:
-
-        if event.type == pygame.MOUSEBUTTONUP:
-            poss = pygame.mouse.get_pos()
-            
-
     # Fill the display with a solid color
     screen.fill("white")
 
+    ScoreBoard = font.render(f"Score: {Score} ", True,(255, 255, 255), (0, 0, 255))
+    screen.blit(ScoreBoard,(100,100))
     # Draw the circle
     pygame.draw.circle(screen, "Purple", pos, 40)
 
